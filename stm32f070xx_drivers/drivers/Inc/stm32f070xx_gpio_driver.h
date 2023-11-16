20611979fb8ede5b1281f918b393e044b8ef766c
Narendran Srinivasan <67863890+iamnarendrans@users.noreply.github.com>
2023-11-16 19:22:01 +0530

    Update stm32f070xx_gpio_driver.h

--- stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h
+++ stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h
@@ -32,7 +32,7 @@ typedef struct{
 typedef struct{
 	// Pointer to hold the base address of the GPIO peripheral
 	GPIO_RegDef_t *pGPIOx;				/*! This holds the base address of the GPIO port to which the pin belongs*/
-	GPIO_PinConfig_t GPIO_PinConfig;	/*! This holds GPIO pin configuration settings*/
+	GPIO_PinConfig_t GPIO_PinConfig;		/*! This holds GPIO pin configuration settings*/
 
 }GPIO_Handle_t;
 
