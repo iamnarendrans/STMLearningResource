0147c613118ee71897d7a3192109755ac5a23e13
iamnarendrans <iamnarendrans@outlook.com>
2023-11-16 19:10:25 +0530

    Created a few more projects and developed a custom GPIO driver

---
+++ stm32f070xx_drivers/Debug/Src/003button_interrupt.d
@@ -0,0 +1,7 @@
+Src/003button_interrupt.o: ../Src/003button_interrupt.c \
+ C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h \
+ C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h \
+ C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h
+C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h:
+C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h:
+C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h:
---
+++ stm32f070xx_drivers/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/stm32f070xx_gpio_driver.c 
+
+OBJS += \
+./drivers/Src/stm32f070xx_gpio_driver.o 
+
+C_DEPS += \
+./drivers/Src/stm32f070xx_gpio_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/%.o drivers/Src/%.su drivers/Src/%.cyclo: ../drivers/Src/%.c drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F070RBTx -DSTM32 -DSTM32F0 -DNUCLEO_F070RB -c -I../Inc -I"C:/Users/Administrator/STM32CubeIDE/workspace_1.13.2/stm32f070xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-drivers-2f-Src
+
+clean-drivers-2f-Src:
+	-$(RM) ./drivers/Src/stm32f070xx_gpio_driver.cyclo ./drivers/Src/stm32f070xx_gpio_driver.d ./drivers/Src/stm32f070xx_gpio_driver.o ./drivers/Src/stm32f070xx_gpio_driver.su
+
+.PHONY: clean-drivers-2f-Src
+
