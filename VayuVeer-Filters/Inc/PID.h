4bbd529d0509ac851517047e09f725e9c68ad727
iamnarendrans <narendran.s@chara.co.in>
2023-12-08 11:57:00 +0530

    did some changes in pid.h and pid.c init and update funtions

---
+++ VayuVeer-Filters/Inc/PID.h
@@ -0,0 +1,46 @@
+/*
+ * PID.h
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: Narendran Srinivasan
+ */
+
+#ifndef PID_H_
+#define PID_H_
+
+
+typedef struct
+{
+	//Controller gains
+	float Kp;
+	float Ki;
+	float Kd;
+
+
+	// Derivative low-pass filter timer constant
+	float tau;
+
+
+	// Output limits
+	float limMin;
+	float limMax;
+
+
+	// Controller memory
+	float integrator;		/* Required for integrator*/
+	float differentiator;	/* Required for differentiator*/
+	float prevError;
+	float prevMeasurement;
+
+
+	// Controller output
+	float out;
+
+}PIDController;
+
+
+void PIDControllerInit(PIDController *pid);
+
+void PIDControllerUpdate(PIDController *pid, float setpoint, float measurement);
+
+#endif /* PID_H_ */
