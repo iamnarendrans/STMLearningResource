cd1069d20c9379f2ce7e28fc57a3248881fbfb16
Narendran Srinivasan <67863890+iamnarendrans@users.noreply.github.com>
2024-01-21 11:47:06 +0530

    Delete stm32f070xx_drivers directory

--- stm32f070xx_drivers/Debug/drivers/Src/stm32f070xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/Src/stm32f070xx_gpio_driver.o: \
- ../drivers/Src/stm32f070xx_gpio_driver.c \
- C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h \
- C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h \
- C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h
-C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h:
-C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx.h:
-C:/Users/iamna/STM32CubeIDE/workspace_1.14.0/stm32f070xx_drivers/drivers/Inc/stm32f070xx_gpio_driver.h:
